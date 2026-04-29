ec3d568ed0728861ff0020b973f0a6f451f46af7
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-04-29 23:05:13 +0300

    feat: add films service

---
+++ src/app/config/app.config.ts
@@ -0,0 +1,6 @@
+export const APP_CONFIG = {
+  useMock: true,
+  apiUrl:
+    'https://cdn.jsdelivr.net/gh/rolling-scopes-school/tasks@master/angular/tasks/angular-intro-task/films.json',
+  mockUrl: 'films.json',
+};
---
+++ src/app/services/films.service.ts
@@ -0,0 +1,49 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { Film } from '../interfaces/film.interface';
+import { APP_CONFIG } from '../config/app.config';
+
+@Injectable({ providedIn: 'root' })
+export class FilmService {
+  films = signal<Film[]>([]);
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  private get url(): string {
+    return APP_CONFIG.useMock ? APP_CONFIG.mockUrl : APP_CONFIG.apiUrl;
+  }
+
+  async loadFilms(): Promise<void> {
+    this.loading.set(true);
+    this.error.set(null);
+
+    try {
+      const res = await fetch(this.url);
+
+      if (!res.ok) {
+        throw new Error('Failed to load movies');
+      }
+
+      const data: Film[] = await res.json();
+
+      this.films.set(data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        this.error.set(error.message);
+      }
+    } finally {
+      this.loading.set(false);
+    }
+  }
+
+  getFilmById(id: number) {
+    return this.films().find((film) => film.id === id);
+  }
+
+  toggleFavorite(id: number) {
+    this.films.update((films) =>
+      films.map((film) => (film.id === id ? { ...film, isFavorite: !film.isFavorite } : film)),
+    );
+  }
+
+  favorites = computed(() => this.films().filter((film) => film.isFavorite));
+}
