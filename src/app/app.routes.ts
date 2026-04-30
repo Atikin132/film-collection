63d32bab5ddc7f6bb90051f73b47e3d31be7cdcc
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-04-30 19:00:23 +0300

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { FilmDetailsPage } from './pages/film-details-page/film-details-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { UnknownPage } from './pages/unknown-page/unknown-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomePage,
+  },
+  {
+    path: 'film/:id',
+    component: FilmDetailsPage,
+  },
+  {
+    path: 'about',
+    component: AboutPage,
+  },
+  {
+    path: '**',
+    component: UnknownPage,
+  },
+];
