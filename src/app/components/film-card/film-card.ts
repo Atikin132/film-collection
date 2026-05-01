f00338054f1b728c6843635392b5c390828e8731
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-01 16:46:11 +0300

    feat: add film card functionality

--- src/app/components/film-card/film-card.ts
+++ src/app/components/film-card/film-card.ts
@@ -1,12 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Film } from '../../interfaces/film.interface';
+import { RouterLink } from '@angular/router';
+import { FilmService } from '../../services/films.service';
 
 @Component({
   selector: 'app-film-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-card.html',
   styleUrl: './film-card.scss',
 })
 export class FilmCard {
   film = input.required<Film>();
+
+  private filmService = inject(FilmService);
+
+  toggleFavorite(event: Event) {
+    event.stopPropagation();
+    this.filmService.toggleFavorite(this.film().id);
+  }
 }
