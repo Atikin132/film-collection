fb2a2d1977e028fe30c152d3e640b4ba8f4b5d1e
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-01 17:54:01 +0300

    feat: add film-details page

--- src/app/pages/film-details-page/film-details-page.ts
+++ src/app/pages/film-details-page/film-details-page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FilmService } from '../../services/films.service';
 
 @Component({
   selector: 'app-film-details-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './film-details-page.html',
   styleUrl: './film-details-page.scss',
 })
-export class FilmDetailsPage {}
+export class FilmDetailsPage {
+  private route = inject(ActivatedRoute);
+  private filmService = inject(FilmService);
+
+  private id = computed(() => Number(this.route.snapshot.paramMap.get('id')));
+
+  film = computed(() => this.filmService.getFilmById(this.id()));
+
+  isLoading = this.filmService.loading;
+
+  isNotFound = computed(() => !this.isLoading() && !this.film());
+}
