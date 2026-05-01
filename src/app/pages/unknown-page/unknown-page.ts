11ac50f43684879f01cc3675903052b8af1fa6ba
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-05-01 19:36:17 +0300

    feat: add unknown page

--- src/app/pages/unknown-page/unknown-page.ts
+++ src/app/pages/unknown-page/unknown-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-unknown-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './unknown-page.html',
   styleUrl: './unknown-page.scss',
 })
